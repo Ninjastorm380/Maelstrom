1868f97b6a60a4d648e363eca7f74a2ca9c088a1
Ninjastorm380 <nanosoftware090@gmail.com>
2021-05-28 23:12:02 -0400

    add ability to disable use of bufers/channels

--- NetworkLibTester/TestServer.vb
+++ NetworkLibTester/TestServer.vb
@@ -1,5 +1,6 @@
 ﻿Public Class TestServer : Inherits Networking.ServerBase
-    Public Overrides Sub Run(Client As Networking.TCPClient)
+    Public Overrides Sub Run(Client As Networking.TcpClient)
+        Client.UseBufferedChannels = False
         Dim Limiter As New Networking.ThreadLimiter(10)
         Do While Client.Connected = True And Online = True
             If Client.HasMessage = True Then
