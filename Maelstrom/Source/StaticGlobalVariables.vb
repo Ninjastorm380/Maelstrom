0a958202b12468f6d15d102593ddd01b90bbcf70
Ninjastorm380 <nanosoftware090@gmail.com>
2022-03-28 01:09:46 -0400

    improve stability with read, write, and seek operations. fine tune default timeouts, and implement large data send mode in internal api.

--- Maelstrom/Source/StaticGlobalVariables.vb
+++ Maelstrom/Source/StaticGlobalVariables.vb
@@ -1,5 +1,5 @@
 Friend Module StaticGlobalVariables
     Friend Const AESBlockSizeBytes as Int32 = 16
-    Friend ReadOnly MaelstromHandshakeVersion as Int32() = {1,0,0,0}
+    Friend ReadOnly MaelstromHandshakeVersion as Int32() = {1,0,0,1}
     Friend ReadOnly MaelstromHandshakeHeader as Byte() = {109, 97, 101, 108, 115, 116, 114, 111, 109, 115, 101, 99, 117, 114, 101, 100}
 End Module
